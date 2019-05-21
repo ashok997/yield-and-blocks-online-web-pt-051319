1afa12fd9cf26955ee21185b1b169932f138b4a5
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-21 16:52:52 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -6,9 +6,7 @@ def hello_t(array)
 end
 
 # call your method here!
-a1=["Tim", "Tom", "Jim"]
-
-hello_t()do|name|
+hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
